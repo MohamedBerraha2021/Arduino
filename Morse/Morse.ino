eba297f212861b9a57fe752a590b9c193d6ae6a1
MohamedBerraha2021 <mohamed.berraha.87@lacetania.cat>
2022-01-31 13:11:01 +0100

    Create Morse.ino

---
+++ Morse/Morse.ino
@@ -0,0 +1,86 @@
+/******************************************************************************
+**                                                                           **
+**                              Morse                                        **
+**                                                                           **
+**                                                                           **
+******************************************************************************/
+
+//****** Includes *************************************************************
+
+//****** Variables ************************************************************
+const int pin = 8;   // pin for speaker
+
+int note = 1047;
+int velocitat = 10;
+
+//****** Setup ****************************************************************
+void setup()
+{
+}
+
+//****** Loop *****************************************************************
+void loop()
+{
+  //M   ** Nom **
+  ratlla();
+  ratlla();
+  //Espai entre lletres
+  espaiL();
+  //O
+  ratlla();
+  ratlla();
+  ratlla();
+  //Espai entre lletres
+  espaiL();
+  //H
+  punt();
+  punt();
+  punt();
+  punt();
+  //Espai entre lletres
+  espaiP();
+  //A   
+  punt();
+  ratlla();
+  //Espai entre lletres
+  espaiL();
+  //M
+  ratlla();
+  ratlla();
+  //Espai entre lletres
+  espaiL();
+  //E
+  punt();
+  //Espai entre lletres
+  //D
+  ratlla();
+  punt();
+  punt();
+  //Espai entre paraules
+  espaiP();
+}
+
+//****** Funcions *************************************************************
+void punt()
+{
+  tone(pin, note, 1000/velocitat);
+  delay(1000/velocitat); 
+  delay(1000/velocitat); 
+}
+
+void ratlla()
+{
+  tone(pin, note, 3*(1000/velocitat));
+  delay(3*(1000/velocitat)); 
+  delay(1000/velocitat);  
+}
+
+void espaiL()  //Espai entre lletres
+{  
+  delay(2*(1000/velocitat));  
+}
+
+void espaiP()  //Espai entre paraules
+{   
+  delay(6*(1000/velocitat));  
+}
